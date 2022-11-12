e4b855d08e6d9f9742d3564864d6401ab717a651
jobgabriel55 <117848600+jobgabriel55@users.noreply.github.com>
2022-11-12 23:40:29 +0300

    Add files via upload

---
+++ Number3.java
@@ -0,0 +1,104 @@
+/* This program was done by 21/04321- Job Gabriel
+Question three: Task one
+*/ 
+package number.pkg3;
+
+import java.util.Scanner;
+
+
+public class Number3 {
+
+   
+    public static void main(String[] args) {
+        //Create an object scanner
+        Scanner input = new Scanner(System.in);
+        //Asks the user to enter a number of their choice
+        System.out.println("Please enter your number of choice:");
+        // Represents the input for it to be readable by the computer
+        int num1=input.nextInt();
+       
+         //Using if statement check whether the number is divisible by 1
+         if (num1%1==0){
+             //If true it prints out the output below
+               System.out.print("\nThe number is divisible by 1" );
+         }
+         else{
+             //if false it prints out the output below
+              System.out.print("\nThe number is not divisible by 1 " );
+         }
+         //Using if statement check whether the number is divisible by 1
+             if (num1%2==0){
+         //If true it prints out the output below
+               System.out.print("\nThe number is divisible by 2" );
+             }
+             else{
+                  //if false it prints out the output below
+                  System.out.print("\nThe number is not divisible by 2" );
+             }
+        //Using if statement check whether the number is divisible by 1
+         if(num1%3==0){
+              //If true it prints out the output below
+               System.out.print("\nThe number is divisible by 3" );
+         }
+    else {
+              //if false it prints out the output below
+     System.out.print("\nThe number is not divisible by 3" );
+         }
+         //Using if statement check whether the number is divisible by 1
+         if(num1%4==0){
+              //If true it prints out the output below
+               System.out.print("\nThe number is divisible by 4" );
+         }
+         else{
+              //if false it prints out the output below
+              System.out.print("\nThe number is not divisible by 4" );
+         }
+      //Using if statement check whether the number is divisible by 1
+          if(num1%5==0){
+               //If true it prints out the output below
+               System.out.print("\nThe number is divisible by 5" );
+       }
+          else{
+               //if false it prints out the output below
+               System.out.print("\nThe number is not divisible by 5" );
+          }
+              //Using if statement check whether the number is divisible by 1
+          if(num1%6==0){
+               //If true it prints out the output below
+               System.out.print("\nThe number is divisible by 6" );
+               }
+          else {
+               //if false it prints out the output below
+               System.out.print("\nThe number is not divisible by 6");
+          }
+               //Using if statement check whether the number is divisible by 1
+          if(num1%7==0){
+               //If true it prints out the output below
+               System.out.print("\nThe number is divisible by 7" );
+               }
+          else{
+               //if false it prints out the output below
+               System.out.print("\nThe number is not divisible by 7" );
+          }
+               //Using if statement check whether the number is divisible by 1
+          if(num1%8==0){
+               //If true it prints out the output below
+               System.out.print("\nThe number is divisible by 8" );
+               }
+          else{
+               //if false it prints out the output below
+               System.out.print("\nThe number is not divisible by 8" );
+          }
+             //Using if statement check whether the number is divisible by 1
+         if(num1%9==0){
+              //If true it prints out the output below
+               System.out.print("\nThe number is divisible by 9" );
+         }
+         else{
+              //if false it prints out the output below
+              System.out.print("\nThe number is not divisible by 9" );
+         }
+         }
+    }
+    
+
